cb19d8d6fbd2e44fdae9ea8eb25fa08664808f97
Frank <f.neumann.berlin@googlemail.com>
2024-04-10 13:39:22 +0200

    mit Layout vom MainFragment

---
+++ app/src/main/java/com/example/orderapp240410/ui/fragment/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.orderapp240410.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.orderapp240410.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    lateinit var binding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+}
