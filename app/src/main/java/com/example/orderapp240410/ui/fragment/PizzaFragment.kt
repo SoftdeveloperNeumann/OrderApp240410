3e99e9a7de8ea5b237977a02509b3d1b3c75b082
Frank <f.neumann.berlin@googlemail.com>
2024-04-11 11:10:13 +0200

    mit RecyclerView und LayoutManagern

--- app/src/main/java/com/example/orderapp240410/ui/fragment/PizzaFragment.kt
+++ app/src/main/java/com/example/orderapp240410/ui/fragment/PizzaFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.orderapp240410.databinding.FragmentPizzaBinding
 import com.example.orderapp240410.model.Pizza
 import com.example.orderapp240410.util.PizzaAdapter
@@ -25,7 +25,9 @@ class PizzaFragment : Fragment() {
         Log.d("TAG", "onCreateView: Das Pizzafragment")
 
         binding.pizzaList.adapter = PizzaAdapter(Pizza.pizzen)
-        binding.pizzaList.layoutManager = LinearLayoutManager(activity)
+//        binding.pizzaList.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL,true)
+//        binding.pizzaList.layoutManager = StaggeredGridLayoutManager(3,LinearLayoutManager.HORIZONTAL)
+        binding.pizzaList.layoutManager = GridLayoutManager(activity,2)
 
         return binding.root
     }
