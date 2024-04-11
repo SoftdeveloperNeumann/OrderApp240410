8fe54c7dd879df7a4e18a4b359b9b639af562ec6
Frank <f.neumann.berlin@googlemail.com>
2024-04-11 08:57:30 +0200

    mit fertigem Tab-Menu und Anzeige des HomeFragment

--- app/src/main/java/com/example/orderapp240410/MainActivity.kt
+++ app/src/main/java/com/example/orderapp240410/MainActivity.kt
@@ -2,17 +2,38 @@ package com.example.orderapp240410
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.orderapp240410.databinding.ActivityMainBinding
+import com.example.orderapp240410.ui.fragment.MainFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var tabNames: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        tabNames = resources.getStringArray(R.array.kategorien)
+
+        binding.pager.adapter = object : FragmentStateAdapter(supportFragmentManager, lifecycle) {
+            override fun getItemCount() = tabNames.size
+
+            override fun createFragment(position: Int): Fragment {
+               return when(position){
+                   0 -> MainFragment()
+                   else -> Fragment()
+               }
+            }
+        }
+        TabLayoutMediator(binding.tabs,binding.pager){tab,position ->
+            tab.text = tabNames[position]
+        }.attach()
 
     }
 }
